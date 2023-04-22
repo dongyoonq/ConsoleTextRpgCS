e80cc1e26c7636d0939cce0703a33ec91368a19f
dongyoonq <ut_community@naver.com>
2023-04-22 11:07:58 +0900

    feat : new Project File

---
+++ Project_S/Project_S/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_S
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
